b7e0e28846df8c46dd7a36e3e5e6d9c3ddba489f
hayato <hayato.pluto.4538@gmail.com>
2021-06-27 17:51:52 +0900

    created avatar image upload function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,14 +14,21 @@ class UserController extends Controller
         return view('users.edit')->with('auth', $auth);
     }
 
-    public function update($id){
-        
+    public function update(Request $request, $id)
+    {
+
         $input = request()->validate([
             'name' => ['required', 'string', 'max:225', 'alpha_dash'],
+            'avatar' => ['file', 'image', 'max:1024'],
             'email' => ['required', 'email', 'max:225']
         ]);
 
         $user = User::findOrFail($id);
+
+        if (request('avatar')) {
+            $input['avatar'] = $request->file('avatar')->store('images');
+        }
+
         $user->update($input);
 
         session()->flash('user-profile-updated-message', 'Your profile was updated successfully.');
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -9,16 +9,32 @@
                 <div class="alert alert-success">{{session('user-profile-updated-message')}}</div>
             @endif
             <h1>Profile</h1>
-            <form action="{{route('user.update', $auth->id)}}" method="POST">
+            <form action="{{route('user.update', $auth->id)}}" method="POST" enctype="multipart/form-data">
                 @csrf
                 @method('PATCH')
+                <img class="rounded" src="{{asset('storage/'.$auth->avatar)}}" alt="avatar" style="width:40%">
+                <div class="form-group">
+                    <input type="file" class="form-control-file mt-2" name="avatar">
+                    
+                    @if($errors->has('avatar'))
+                        <p class="text-danger">{{$errors->first('avatar')}}</p>
+                    @endif
+                </div>
                 <div class="form-group">
                     <label>Name</label>
-                    <input type="text" class="form-control" name="name" value="{{$auth->name}}">
+                    <input type="text" class="form-control" name="name" value="{{old('name',$auth->name)}}">
+                    
+                    @if($errors->has('name'))
+                        <p class="text-danger">{{$errors->first('name')}}</p>
+                    @endif
                 </div>
                 <div class="form-group">
                     <label>Email address</label>
-                    <input type="email" class="form-control" name="email" value="{{$auth->email}}">
+                    <input type="email" class="form-control" name="email" value="{{old('email',$auth->email)}}">
+                    
+                    @if($errors->has('email'))
+                        <p class="text-danger">{{$errors->first('email')}}</p>
+                    @endif
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
