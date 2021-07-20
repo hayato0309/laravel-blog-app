e9d4b513b5350de7bf6f812e8b4d10552452436b
hayato <hayato.pluto.4538@gmail.com>
2021-07-20 10:58:18 +0900

    created the link to admin page on header for admin user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,6 +63,15 @@
                                     <a class="dropdown-item" href="{{ route('user.show', Auth::user()->id) }}">
                                         Profile
                                     </a>
+
+                                    @foreach(Auth::user()->roles as $role)
+                                        @if($role->slug == 'admin')
+                                            <a class="dropdown-item" href="{{ route('admin.home', Auth::user()) }}">
+                                                Admin
+                                            </a>
+                                        @endif
+                                    @endforeach
+                                    
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
