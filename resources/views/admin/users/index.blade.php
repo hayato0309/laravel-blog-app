2b4ca08ac5def5b49fe63a32771e0fb7835831cd
hayato <hayato.pluto.4538@gmail.com>
2021-07-21 23:31:20 +0900

    adjusted the paddings in modals on my post page and posts page for admin

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -87,7 +87,7 @@
                             <div class="modal-dialog" role="document">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">Deactivate confirmation</h5>
+                                        <h5 class="modal-title" id="exampleModalLabel">Activate confirmation</h5>
                                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                         </button>
