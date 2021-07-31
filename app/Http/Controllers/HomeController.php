e7aa63b7911c769f85fee566bc949c4174c18327
hayato0309 <hayato.pluto.4538@gmail.com>
2021-07-31 22:53:36 +0900

    display news on home page with News API

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,9 @@ use App\Post;
 use App\User;
 use App\Like;
 use App\Category;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7;
 
 class HomeController extends Controller
 {
@@ -51,12 +54,40 @@ class HomeController extends Controller
         // Create query instance
         $query = Post::query();
 
-        // If there is a category search keywaord...
+        // If there is a category search keyword...
         if (!empty($post_search)) {
             $query->where('title', 'like', '%' . $post_search . '%');
             $posts = $query->paginate(5);
         }
 
-        return view('home', compact('post_search', 'categories', 'posts'));
+        // Get news with News API
+        try {
+            $url = config('newsapi.news_api_url') . "everything?q=violinist&apiKey=" . config('newsapi.news_api_key');
+            $method = "GET";
+            $count = 5;
+
+            $client = new Client();
+            $response = $client->request($method, $url);
+
+            $results = $response->getBody();
+            $articles = json_decode($results, true);
+
+            $news_list = [];
+
+            for ($id = 0; $id < $count; $id++) {
+                array_push($news_list, [
+                    'title' => $articles['articles'][$id]['title'],
+                    'url' => $articles['articles'][$id]['url'],
+                    'thumbnail' => $articles['articles'][$id]['urlToImage'],
+                ]);
+            }
+        } catch (RequestException $e) {
+            echo Psr7\Message::toString($e->getRequest());
+            if ($e->hasResponse()) {
+                echo Psr7\Message::toString($e->getResponse());
+            }
+        }
+
+        return view('home', compact('post_search', 'categories', 'posts', 'news_list'));
     }
 }
