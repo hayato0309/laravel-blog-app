165eed7f8b1ab6324ea71733eb86796dba6d48d4
hayato0309 <hayato.pluto.4538@gmail.com>
2021-08-01 12:57:49 +0900

    store post type data by admin

--- app/Http/Controllers/PostTypeController.php
+++ app/Http/Controllers/PostTypeController.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\PostType;
+use Illuminate\Support\Str;
 
 class PostTypeController extends Controller
 {
     public function index()
     {
         return view('admin.postTypes.index');
+    }
+
+    public function store()
+    {
+        $input = request()->validate([
+            'name' => ['required', 'min:3', 'max:30', 'unique:post_types'],
+        ]);
+
+        $post_type = new PostType();
+        $post_type->name = Str::ucfirst($input['name']);
+        $post_type->slug = Str::slug($input['name']);
+
+        $post_type->save();
+
+        session()->flash('post-type-created-message', 'Post type was created successfully. : ' . $post_type->name);
+
+        return back();
     }
 }
--- database/migrations/2021_08_01_033302_create_post_types_table.php
+++ database/migrations/2021_08_01_033302_create_post_types_table.php
@@ -15,6 +15,8 @@ class CreatePostTypesTable extends Migration
     {
         Schema::create('post_types', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
