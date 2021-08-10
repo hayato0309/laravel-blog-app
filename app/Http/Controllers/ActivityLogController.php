52b43e56c8eab8a5d73de9ee0fcfce1f8fe51282
hayato0309 <hayato.pluto.4538@gmail.com>
2021-08-11 08:27:53 +0900

    send notification when following users post

--- app/Http/Controllers/ActivityLogController.php
+++ app/Http/Controllers/ActivityLogController.php
@@ -3,24 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use App\User;
+use App\Post;
 
 class ActivityLogController extends Controller
 {
     public function index()
     {
-        // Get data from notification table only for the user has the post.
-        $activity_logs = DB::table('notifications')
-            ->join('posts', 'notifications.notifiable_id', '=', 'posts.id')
-            ->where('notifications.notifiable_type', 'App\Post')
-            ->where('posts.user_id', Auth()->user()->id)
-            ->get();
+        // notifiable_idが自分のIDのnotificationを全部取得
+        $activity_logs = auth()->user()->notifications;
+
+        // Activity Logのコレクションをforeachで追加するための空コレクション
+        $activity_logs_for_auth = collect();
+
+        // Activity Log配列を作成するためのキーを定義
+        $key_for_activity_logs_collection = 0;
 
         foreach ($activity_logs as $activity_log) {
-            $activity_log->data = json_decode(($activity_log->data));
+            // ログインユーザIDとNotificationテーブルにデータを発行したPostのUser IDが同じ時だけ、Activity Logとして取得
+
+            if (auth()->user()->id === Post::find($activity_log->data['id'])->user_id) {
+                $activity_logs_for_auth = $activity_logs_for_auth->concat(collect([$key_for_activity_logs_collection => $activity_log]));
+                $key_for_activity_logs_collection += 1;
+            }
         }
 
-        return view('activity_logs.index', compact('activity_logs'));
+        return view('activity_logs.index', compact('activity_logs_for_auth'));
     }
 }
