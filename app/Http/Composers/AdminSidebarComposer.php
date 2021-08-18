3da0b6e2ed998a3af05962608ca0e7f37e975748
hayato0309 <hayato.pluto.4538@gmail.com>
2021-08-19 07:45:07 +0900

    display the num of unsolved inquiries on sidebar on admin page

--- app/Http/Composers/AdminSidebarComposer.php
+++ app/Http/Composers/AdminSidebarComposer.php
@@ -3,15 +3,21 @@
 namespace App\Http\Composers;
 
 use Illuminate\View\View;
+use App\Inquiry;
 
 class AdminSidebarComposer
 {
     public function compose(View $view)
     {
+        // 未読のnotificationsをカウント
         $num_of_unread_notifications_for_admin = auth()->user()->unreadNotifications()
             ->where('type', 'App\Notifications\UserRegisteredNotification')
             ->count();
 
-        $view->with('num_of_unread_notifications_for_admin', $num_of_unread_notifications_for_admin);
+        // 未解決のinquiriesをカウント
+        $num_of_unsolved_inquiries = Inquiry::where('is_solved', 0)->count();
+
+        $view->with('num_of_unread_notifications_for_admin', $num_of_unread_notifications_for_admin)
+            ->with('num_of_unsolved_inquiries', $num_of_unsolved_inquiries);
     }
 }
